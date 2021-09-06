00612f254087324c496876486f23ab23479e87ba
Djavani Gomes <djavanirgomes@gmail.com>
2021-09-06 11:20:49 -0300

    Primeiro Commit - modelo da pagina com dados mocados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/github.png";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">Github profile</h1>
+      <form>
+        <div className="form-group">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="GitHub User"
+              className="form-contropl"
+              required
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        <img
+          src={logo}
+          className="responsive rounded-circle"
+          alt="Logomarca Github"
+          height="200px"
+        />
+      </div>
+      <h1 className="pt-5">
+        <a href="https://github.com/djavani" target="_new">
+          Djavani Gomes
         </a>
-      </header>
+      </h1>
+      <h3>Cataguases</h3>
+      <p>
+        <a href="https://www.youtube.com/" target="_new" className="text-info">
+          https://www.youtube.com/
+        </a>
+      </p>
     </div>
   );
 }
