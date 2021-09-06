d390c8b7888081e7735f53d9ae8511b5754adf8a
Djavani Gomes <djavanirgomes@gmail.com>
2021-09-06 16:48:56 -0300

    Consumindo dados da API Github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
   const [search, setSearch] = useState("");
   const [userData, setUserData] = useState("");
 
+  
+  
+
+    
+    let dataInicioMenbro =  new Date(userData.created_at);    
+    let dataultimoAcesso = new Date(userData.updated_at);
+    const dataInicioFormatada = dataInicioMenbro.getDate() + "/"+ (dataInicioMenbro.getMonth() + 1) + "/"+ dataInicioMenbro.getFullYear()
+    const dataUtimoAcessoFormatada = dataultimoAcesso.getDate() + "/"+ (dataultimoAcesso.getMonth() + 1)+ "/"+ dataultimoAcesso.getFullYear()
+  
+
+
   const handleChange = (event) => {
     setSearch(event.target.value);
   };
@@ -14,68 +25,72 @@ function App() {
     event.preventDefault();
     fetch(`https://api.github.com/users/${search}`)
       .then((response) => response.json())
-      .then((userResposne) => setUserData(userResposne));
-  };
-
-  //console.log(userData);
+      .then((userResposne) => setUserData(userResposne))
+      
+  };  
 
   return (
-    <div className="container text-center">
-      <h1 className="py-5 text-uppercase">Github profile</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <div className="input-group">
-            <input
-              type="text"
-              placeholder="GitHub User"
-              className="form-contropl"
-              required
-              value={search}
-              onChange={handleChange}
-            />
-            <span className="input-group-btn">
-              <button type="submit" className="btn btn-success">
-                Search
-              </button>
-            </span>
+    <div className="fundo">
+      <div className="container text-center">
+        {/* <div className="container text-center display: flex" > */}
+        <h1 className="py-5 text-uppercase">Github profile</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <div className="input-group">
+              <div className="search">
+                <input
+                  type="text"
+                  placeholder="GitHub User"
+                  className="form-contropl"
+                  required
+                  value={search}
+                  onChange={handleChange}
+                />
+                <span className="input-group-btn">
+                  <button type="submit" className="btn btn-success">
+                    Search
+                  </button>
+                </span>
+              </div>
+            </div>
           </div>
-        </div>
-      </form>
-      <div className="py-5">
-        {!userData && (
-          <img
-            src={logo}
-            className="responsive rounded-circle"
-            alt="Logomarca Github"
-            height="200px"
-          />
-        )}
-        {userData && (
-          <div>
+        </form>
+        <div className="py-5">
+          {!userData && (
             <img
-              src={userData.avatar_url}
+              src={logo}
               className="responsive rounded-circle"
               alt="Logomarca Github"
               height="200px"
-            />            
-            <h1 className="pt-5">
-              <a href={userData.html_url} target="_new">
-                {userData.name}
-              </a>
-            </h1>
-            <span>ID: {userData.id}</span>
-            <h3>{userData.bio}</h3>
-            <p>
-              <a
-                href="https://www.youtube.com/"
-                target="_new"
-                className="text-info"
-              >
-                 {userData.location}
-              </a>
-            </p>
-          </div>
-        )}
+            />
+          )}
+          {userData && (
+            <div className="perfil">
+              <img
+                src={userData.avatar_url}
+                className="responsive rounded-circle"
+                alt="Logomarca Github"
+                height="200px"
+                
+              />
+              <h1 className="pt-5">
+                <a href={userData.html_url} target="_new">
+                  {userData.name}
+                </a>
+              </h1>
+              <span>ID: {userData.id}</span>
+              <h5>{userData.bio}</h5>
+              <p className="paragrafo"> Empresa: <strong>{!userData.company ? 'N/D': userData.company}</strong> </p>
+              <p className="paragrafo"> Login: <strong> {userData.login} </strong></p>
+              <p className="paragrafo"> Localização: <strong>{userData.location}</strong></p>
+              <p className="paragrafo"> Membro desde: {dataInicioFormatada} </p>
+              <p className="paragrafo"> Seguidores: {userData.followers} </p>
+              <p className="paragrafo"> Seguindo: {userData.following} </p>
+              <p className="paragrafo"> N Repositórios públicos: {userData.public_repos} </p>
+              <p className="paragrafo"> Último acesso: {dataUtimoAcessoFormatada} </p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
